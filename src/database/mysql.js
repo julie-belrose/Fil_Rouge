97d0f882d78053df6650a194d4508362bab23e90
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2025-06-27 15:26:16 +0200

    feat(mongodb.js, mysql.js): add just basiic file configuration

---
+++ src/database/mysql.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql2/promise');
+const logger = require('../utils/logger');
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+// Test de la connexion
+const testConnection = async () => {
+  try {
+    const connection = await pool.getConnection();
+    logger.info('MySQL Connected');
+    connection.release();
+  } catch (error) {
+    logger.error();
+    process.exit(1);
+  }
+};
+
+module.exports = {
+  pool,
+  testConnection
+};
